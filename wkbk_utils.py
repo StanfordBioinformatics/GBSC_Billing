beb8e346e57715a8aa060093ac80313d3bd2cd6e
Keith Bettinger <bettingr@stanford.edu>
2013-11-05 12:48:46 -0800

    Removing auxiliary files.

--- wkbk_utils.py
+++ wkbk_utils.py
@@ -2,7 +2,8 @@
 
 #===============================================================================
 #
-# NAMEOFSCRIPT.py - Description of script
+# wkbk_utils.py - Set of utilities to help in using workbooks
+#                   from xlrd and Xlsxwriter.
 #
 # ARGS:
 #
@@ -27,7 +28,9 @@ import os
 import os.path
 import sys
 
-from ogetools import accounting
+import xlrd
+
+import xlsxwriter
 
 #=====
 #
@@ -41,6 +44,7 @@ from ogetools import accounting
 #
 #=====
 
+
 #=====
 #
 # SCRIPT BODY
@@ -49,24 +53,10 @@ from ogetools import accounting
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("acc_file")
 parser.add_argument("-v", "--verbose", action="store_true",
                     default=False,
                     help='Get real chatty [default = false]')
 
 opts = parser.parse_args()
 
-jobstats = accounting.Accounting(opts.acc_file) #, "QLOGIN")
-
-for job in jobstats.getColumns('job_number', 'ru_wallclock', 'ru_utime', 'ru_stime', 'slots'):
-    job_id = job[0]
-    ru_wallclock = float(job[1])
-    ru_utime     = float(job[2])
-    ru_stime     = float(job[3])
-    wall_hours = ru_wallclock/60/60
-
-    slots        = int(job[4])
-
-    if ru_wallclock > 0.0:
-        print job_id, wall_hours, ru_wallclock, ru_utime, ru_stime, (ru_wallclock - ru_utime - ru_stime)/ru_wallclock*100.0, slots
 
