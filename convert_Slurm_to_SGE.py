65d7e7ca3c03247e4de74f68fa2d395b70e1ba46
Keith Bettinger <bettingr@stanford.edu>
2018-03-15 17:14:28 -0700

    Removed unused import statements. Added missing fields to Dialects. Registed Dialects as "slurm" and "sge". Fixed conversion of Slurm time strings to timestamps. Put in output CSVWriter for SGE. Supported MaxDiskRead/Write if they are null. Added progress output to STDERR. Tightened up code around file/dir paths.

--- convert_Slurm_to_SGE.py
+++ convert_Slurm_to_SGE.py
@@ -44,19 +44,12 @@
 #
 #=====
 import argparse
-import codecs
-import collections
+import calendar
 import csv
-import datetime
-import locale
-import os
-import os.path
-import subprocess
-import sys
 import time
+import os.path
+import sys
 
-import xlrd
-import xlsxwriter
 
 # Simulate an "include billing_common.py".
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -78,24 +71,33 @@ class SlurmDialect(csv.Dialect):
     delimiter = '|'
     doublequote = False
     escapechar = '\\'
+    lineterminator = '\n'
+    quotechar = '"'
     quoting = csv.QUOTE_MINIMAL
     skipinitialspace = True
     strict = True
+csv.register_dialect("slurm",SlurmDialect)
 
 class SGEDialect(csv.Dialect):
 
     delimiter = ':'
     doublequote = False
     escapechar = '\\'
+    lineterminator = '\n'
+    quotechar = '"'
     quoting = csv.QUOTE_MINIMAL
     skipinitialspace = True
     strict = True
+csv.register_dialect("sge",SGEDialect)
 
 #=====
 #
 # GLOBALS
 #
 #=====
+
+# In billing_common.py
+global ACCOUNTING_FIELDS
 
 #=====
 #
@@ -107,49 +109,80 @@ class SGEDialect(csv.Dialect):
 
 def convert_slurm_file_to_sge_file(slurm_fp, sge_fp):
 
+    # Read in the header line from the Slurm file to use for the DictReader
     header = slurm_fp.readline()
     fieldnames = header.split('|')
+    reader = csv.DictReader(slurm_fp,fieldnames=fieldnames,dialect="slurm")
 
-    reader = csv.DictReader(slurm_fp,fieldnames=fieldnames,dialect=SlurmDialect())
+    writer = csv.DictWriter(sge_fp,fieldnames=ACCOUNTING_FIELDS,dialect="sge")
+    sge_row = {}  # Create new dictionary for the output row.
+
+    line_num = 0
     for slurm_row in reader:
 
-        qname = slurm_row['Partition']
-        hostname = slurm_row['NodeList']
-        group = slurm_row['Group']
-        owner = slurm_row['User']
-        job_name = slurm_row['JobName']
-        job_number = slurm_row['JobIDRaw']
-        account = slurm_row['Account']
-        submission_time = int(datetime.datetime.strptime(slurm_row['Submit'],"%Y-%m-%dT%H:%M:%S").timestamp())
-        start_time = int(datetime.datetime.strptime(slurm_row['Start'],"%Y-%m-%dT%H:%M:%S").timestamp())
-        end_time = int(datetime.datetime.strptime(slurm_row['End'],"%Y-%m-%dT%H:%M:%S").timestamp())
-        failed = 0  # TODO: convert Slurm states to SGE failed states
+        try:
+            sge_row.clear()
 
-        (return_value, signal) = slurm_row['ExitCode'].split(':')
-        if signal == 0:
-            exit_status = return_value
-        else:
-            exit_status = 128 + signal
+            sge_row['qname'] = slurm_row['Partition']
+            sge_row['hostname'] = slurm_row['NodeList']
+            sge_row['group'] = slurm_row['Group']
+            sge_row['owner'] = slurm_row['User']
+            sge_row['job_name'] = slurm_row['JobName']
+            sge_row['job_number'] = slurm_row['JobIDRaw']
+            sge_row['account'] = slurm_row['Account']
 
-        ru_wallclock = slurm_row['Elapsed']
+            sge_row['submission_time'] = calendar.timegm(time.strptime(slurm_row['Submit'],"%Y-%m-%dT%H:%M:%S"))
+            sge_row['start_time'] = calendar.timegm(time.strptime(slurm_row['Start'],"%Y-%m-%dT%H:%M:%S"))
+            sge_row['end_time'] = calendar.timegm(time.strptime(slurm_row['End'],"%Y-%m-%dT%H:%M:%S"))
 
-        project = slurm_row['WCKey']
-        department = "NoDept"
-        granted_pe = "NoPE"
-        slots = slurm_row['NCPUS']
+            sge_row['failed'] = 0  # TODO: convert Slurm states to SGE failed states
 
-        cpu = slurm_row['CPUTime']
+            (return_value, signal) = slurm_row['ExitCode'].split(':')
+            if signal == 0:
+                sge_row['exit_status'] = int(return_value)
+            else:
+                sge_row['exit_status'] = 128 + int(signal)
 
-        io = int(slurm_row['MaxDiskRead']) + int(slurm_row['MaxDiskWrite'])
-        category = slurm_row['ReqGRES']
+            # Convert Elapsed of form DD-HH:MM:SS to seconds
+            elapsed_days_split = slurm_row['Elapsed'].split('-')
+            if len(elapsed_days_split) == 1:
+                elapsed_days = 0
+                elapsed_hms  = elapsed_days_split[0]
+            elif len(elapsed_days_split) == 2:
+                elapsed_days = int(elapsed_days_split[0])
+                elapsed_hms  = elapsed_days_split[1]
+            else:
+                print >> sys.stderr, "Elapsed time of", slurm_row['Elapsed'], "is malformed."
 
-        maxvmem = slurm_row['MaxVMSize']
+            elapsed_seconds = (elapsed_days * 86400) + sum(int(x) * 60 ** i for i,x in enumerate(reversed(elapsed_hms.split(":"))))
 
+            sge_row['ru_wallclock'] = elapsed_seconds
 
+            sge_row['project'] = slurm_row['WCKey']
+            sge_row['department'] = "NoDept"
+            sge_row['granted_pe'] = "NoPE"
+            sge_row['slots'] = slurm_row['NCPUS']
 
+            sge_row['cpu'] = slurm_row['CPUTimeRAW']
 
+            if slurm_row['MaxDiskRead'] != '':
+                sge_row['io'] = int(slurm_row['MaxDiskRead'])
+            if slurm_row['MaxDiskWrite'] != '':
+                sge_row['io'] += int(slurm_row['MaxDiskWrite'])
+            sge_row['category'] = slurm_row['ReqGRES']
 
+            sge_row['max_vmem'] = slurm_row['MaxVMSize']
+        except ValueError:
+            print sge_row
 
+        # Output row to SGE file.
+        writer.writerow(sge_row)
+
+        line_num += 1
+        if line_num % 10000 == 0:
+            print >> sys.stderr, ".",
+
+    print >> sys.stderr
 
 #=====
 #
@@ -162,9 +195,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("slurm_accounting_file",
                     default=None,
                     help='The Slurm accounting file to read [default = stdin]')
-parser.add_argument("-o", "sge_accounting_file",
+parser.add_argument("sge_accounting_file",
                     default=None,
                     help='The SGE accounting file to output to [default = stdout]')
+parser.add_argument("-r", "--billing_root",
+                    default=None,
+                    help='The Billing Root directory [default = None]')
 parser.add_argument("-v", "--verbose", action="store_true",
                     default=False,
                     help='Get real chatty [default = false]')
@@ -178,8 +214,8 @@ args = parser.parse_args()
 # Override billing_root with switch args, if present.
 if args.billing_root is not None:
     billing_root = args.billing_root
-# If we still don't have a billing root dir, use the current directory.
-if billing_root is None:
+else:
+    # Use the current directory.
     billing_root = os.getcwd()
 
 # Get absolute path for billing_root directory.
@@ -187,19 +223,14 @@ billing_root = os.path.abspath(billing_root)
 
 # Use switch arg for accounting_file if present, else use file in BillingRoot.
 if args.slurm_accounting_file is not None:
-    slurm_accounting_file = args.slurm_accounting_file
+    slurm_accounting_file = os.path.abspath(args.slurm_accounting_file)
 else:
     slurm_accounting_file = "STDIN"
 
-if args.sge_accounting_file is not None
-    sge_accounting_file = args.sge_accounting_file
+if args.sge_accounting_file is not None:
+    sge_accounting_file = os.path.abspath(args.sge_accounting_file)
 else:
     sge_accounting_file = "STDOUT"
-
-# Get absolute path for accounting files.
-slurm_accounting_file = os.path.abspath(slurm_accounting_file)
-sge_accounting_file = os.path.abspath(sge_accounting_file)
-
 #
 # Output the state of arguments.
 #
