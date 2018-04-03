fcdc8bf0f65e5d5df5fc1c9cac3a125d67cea3c6
Keith Bettinger <bettingr@stanford.edu>
2018-04-03 12:19:41 -0700

    Added new column "Node" to BillingNotifications Computing Details spreadsheet headers.

--- billing_common.py
+++ billing_common.py
@@ -85,7 +85,7 @@ BILLING_DETAILS_SHEET_COLUMNS = OrderedDict((
 BILLING_NOTIFS_SHEET_COLUMNS = OrderedDict( (
     ('Billing',   () ),  # Billing sheet is not columnar.
     ('Lab Users', ('Username', 'Full Name', 'Email', 'Date Added', 'Date Removed') ),
-    ('Computing Details' , ('Job Date', 'Username', 'Job Name', 'Job Tag', 'CPU-core Hours', 'Job ID', '%age') ),
+    ('Computing Details' , ('Job Date', 'Username', 'Job Name', 'Job Tag', 'Node', 'CPU-core Hours', 'Job ID', '%age') ),
     ('Cloud Details', ('Platform', 'Project', 'Description', 'Dates', 'Quantity', 'Unit of Measure', 'Charge', '%age', 'Lab Cost') ),
     ('Consulting Details', ('Date', 'Summary', 'Notes', 'Participants', 'Hours', 'Travel Hours', 'Cumul Hours')),
     ('Rates'      , ('Type', 'Amount', 'Unit', 'Time' ) )
