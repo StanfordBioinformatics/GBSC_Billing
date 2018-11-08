c02b5239c3e904644c1569ab5bb8bbc9172808a7
bettingr <bettingr@stanford.edu>
2018-11-08 15:18:58 -0800

    HACK FOR SEPT 2018: because of a misconfiguration in Slurm, jobs which asked for 1 CPU often got 2.  The easiest way to solve this error was to use ReqCPUS field instead of NCPUS to get the CPU count used, for just this month.

--- slurm_job_accounting_entry.py
+++ slurm_job_accounting_entry.py
@@ -16,7 +16,8 @@ class SlurmJobAccountingEntry(job_accounting_entry.JobAccountingEntry):
         self.account = slurm_line_dict['Account']
         self.project = slurm_line_dict['WCKey']  # for future development
         self.node_list = slurm_line_dict['NodeList']
-        self.cpus = self.dict_get_int(slurm_line_dict, 'NCPUS')
+        # HACK FOR SEPT 2018: NCPUS was broken in this month by a misconfiguration in Slurm; ReqCPUS has the right data.
+        self.cpus = self.dict_get_int(slurm_line_dict, 'ReqCPUS')
         self.wallclock = self.dict_get_int(slurm_line_dict, 'ElapsedRaw')
         self.job_id = self.dict_get_int(slurm_line_dict, 'JobIDRaw')
 
