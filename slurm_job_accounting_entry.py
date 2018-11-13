45fe7eec53e2fed5d766c754c4781eddda59a0eb
Keith Bettinger <bettingr@stanford.edu>
2018-11-12 17:13:13 -0800

    Removing hack for Sept 2018 billing: restoring CPU count to come from "NCPUS".

--- slurm_job_accounting_entry.py
+++ slurm_job_accounting_entry.py
@@ -16,8 +16,7 @@ class SlurmJobAccountingEntry(job_accounting_entry.JobAccountingEntry):
         self.account = slurm_line_dict['Account']
         self.project = slurm_line_dict['WCKey']  # for future development
         self.node_list = slurm_line_dict['NodeList']
-        # HACK FOR SEPT 2018: NCPUS was broken in this month by a misconfiguration in Slurm; ReqCPUS has the right data.
-        self.cpus = self.dict_get_int(slurm_line_dict, 'ReqCPUS')
+        self.cpus = self.dict_get_int(slurm_line_dict, 'NCPUS')
         self.wallclock = self.dict_get_int(slurm_line_dict, 'ElapsedRaw')
         self.job_id = self.dict_get_int(slurm_line_dict, 'JobIDRaw')
 
